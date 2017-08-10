3fb4e97f906931225b7e6eda9903f945232f03ad
Max W <xypotion@gmail.com>
2017-08-10 15:44:57 -0700

    Micropost scaffolding; some validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    # @micropost 
   end
 
   # GET /users/new
@@ -53,7 +54,7 @@ class UsersController < ApplicationController
 
   # DELETE /users/1
   # DELETE /users/1.json
-  private def destroy
+  def destroy
     @user.destroy
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
